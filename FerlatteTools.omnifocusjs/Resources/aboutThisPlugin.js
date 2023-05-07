48df3c2984d65911b46afa36fa014f3609189aee
Mark Ferlatte <mark@truss.works>
2023-05-07 11:25:48 -0700

    Add about this plugin

---
+++ FerlatteTools.omnifocusjs/Resources/aboutThisPlugin.js
@@ -0,0 +1,18 @@
+/* global PlugIn, Alert */
+(() => {
+  let action = new PlugIn.Action(function() {
+    let alertTitle = `${this.plugIn.displayName} v${this.plugIn.version.versionString}`;
+    let alertMessage = `${this.plugIn.author}
+${this.plugIn.identifier}
+
+https://github.com/ferlatte/omnifocus-plugins
+
+${this.plugIn.description}
+`;
+    let alert = new Alert(alertTitle, alertMessage);
+    alert.addOption("OK");
+    alert.show();
+  });
+
+  return action;
+})();
