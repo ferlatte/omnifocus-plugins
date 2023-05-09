922c571f92a8a9baf68a3887423455aad4ab1ff0
Mark Ferlatte <mark@truss.works>
2023-05-08 17:13:02 -0700

    Refactor OpenAI API into shared library.

--- FerlatteTools.omnifocusjs/Resources/FerlatteLib.js
+++ FerlatteTools.omnifocusjs/Resources/FerlatteLib.js
@@ -1,4 +1,4 @@
-/* global Alert, Calendar, DateComponents, PlugIn, URL, Version */
+/* global Alert, Calendar, console, DateComponents, Form, PlugIn, URL, Version */
 (() => {
   const lib = new PlugIn.Library(new Version("0.1"));
 
@@ -45,6 +45,66 @@
     }
     return undefined;
   };
+
+  lib.requestOpenAIApiKey = async function(credentials) {
+    const API_KEY_FIELD_KEY = "apiKey";
+    let inputForm = new Form();
+    let apiKeyField = new Form.Field.String(
+      API_KEY_FIELD_KEY,
+      "OpenAI API Key"
+    );
+    inputForm.addField(apiKeyField);
+
+    inputForm.validate = formObject => {
+      let apiKey = formObject.values[API_KEY_FIELD_KEY];
+      return !! (apiKey && apiKey.length > 0);
+    };
+    let formPrompt = `API key for ${lib.openAIServiceIdentifier}`;
+    try {
+      let formObject = await inputForm.show(formPrompt, "Continue");
+      let apiKey = formObject.values[API_KEY_FIELD_KEY];
+      // The Credentials API requires a username; it will let you write
+      // just a password, but then reads will fail.
+      credentials.write(lib.openAIServiceIdentifier, "BearerToken", apiKey);
+    } catch(e) {
+      console.log(e);
+    }
+  };
+
+  lib.requestOpenAIApiKeyRemoval = async function(credentials) {
+    let alert = new Alert("Confirmation Required",
+                          "Remove stored OpenAI API Key?");
+    alert.addOption("Remove");
+    alert.addOption("Cancel");
+    let buttonIndex = await alert.show();
+    if (buttonIndex === 0) {
+      credentials.remove(lib.openAIServiceIdentifier);
+    }
+  };
+
+  lib.openAIChatCompletion = async function(apiKey, requestBody) {
+    const openAICompletionURL = "https://api.openai.com/v1/chat/completions";
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`
+    };
+    let r = undefined;
+    let request = URL.FetchRequest.fromString(openAICompletionURL);
+    request.method = "POST";
+    request.headers = headers;
+    request.bodyString = JSON.stringify(requestBody);
+    let response = await request.fetch();
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      r = JSON.parse(response.bodyString);
+    } else {
+      if (response.mimeType === "application/json") {
+        let errorMessage = JSON.parse(response.bodyString).message;
+        throw new Error(errorMessage);
+      }
+    }
+    return r;
+  };
+
 
   return lib;
 })();
--- FerlatteTools.omnifocusjs/Resources/estimateWithMagic.js
+++ FerlatteTools.omnifocusjs/Resources/estimateWithMagic.js
@@ -1,15 +1,15 @@
-/* global PlugIn, Credentials, URL, Form, Alert, app, console */
+/* global PlugIn, Credentials, Alert, app, console */
 (() => {
   // Credentials can only be created at plugin load.
   let credentials = new Credentials();
-  const serviceIdentifier = "OpenAI";
+
+  let FerlatteLib = undefined;
 
   function alertError(e) {
     return new Alert("Error", e.message);
   }
 
-  function setEstimatedDuration(apiKey, task) {
-    const openaiCompletionURL = "https://api.openai.com/v1/chat/completions";
+  async function setEstimatedDuration(apiKey, task) {
     const requestBody = {
       model: "gpt-3.5-turbo",
       messages: [
@@ -29,94 +29,34 @@
       temperature: 0.7,
       stop: "\n"
     };
-    const headers = {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${apiKey}`
-    };
-    let request = URL.FetchRequest.fromString(openaiCompletionURL);
-    request.method = "POST";
-    request.headers = headers;
     try {
-      request.bodyString = JSON.stringify(requestBody);
-    } catch(e) {
-      console.log("error: ", e);
+      let r = await FerlatteLib.openAIChatCompletion(apiKey, requestBody);
+      let retval = r?.choices[0]?.message?.content;
+      if (retval) {
+        let minutes = Number(retval);
+        if (Number.isInteger(minutes)) {
+          task.estimatedMinutes = Number(retval);
+        } else {
+          throw new Error("Invalid number of minutes returned: ", retval);
+        }
+      } else {
+        throw new Error(r);
+      }
+    } catch (e) {
+      console.log(e);
       alertError(e).show();
     }
-    request.fetch().then(
-      response => {
-        if (response.statusCode >= 200 && response.statusCode < 300) {
-          let r = JSON.parse(response.bodyString);
-          let retval = r?.choices[0]?.message?.content;
-          if (retval) {
-            let minutes = Number(retval);
-            if (Number.isInteger(minutes)) {
-              task.estimatedMinutes = Number(retval);
-            } else {
-              throw new Error("Invalid number of minutes returned: ", retval);
-            }
-          } else {
-            throw new Error(response.bodyString);
-          }
-        } else {
-          if (response.mimeType === "application/json") {
-            let errorMessage = JSON.parse(response.bodyString).message;
-            throw new Error(errorMessage);
-          }
-        }
-      }
-    ).catch((e) => {
-      console.log("error: ", e);
-      alertError(e).show();
-    });
   }
 
-  function requestAPIKey() {
-    let inputForm = new Form();
-    let apiKeyField = new Form.Field.String(
-      "apiKey",
-      "OpenAI API Key",
-      null
-    );
-    inputForm.addField(apiKeyField);
-
-    inputForm.validate = formObject => {
-      let apiKey = formObject.values["apiKey"];
-      let apiKeyStatus = (apiKey && apiKey.length > 0);
-      return !! apiKeyStatus;
-    };
-    let formPrompt = `API Key for ${serviceIdentifier}:`;
-    inputForm.show(formPrompt, "Continue").then(formObject => {
-      let apiKey = formObject.values["apiKey"];
-      // NB: The Credentials API requires a username; it will let you
-      // write just a password, but reads will then fail.
-      let c = credentials.write(serviceIdentifier, "BearerToken", apiKey);
-      if (! c) {
-        throw new Error("Credentials.write() failed");
-      }
-    }).catch((e) => {
-      console.log("error: ", e);
-      alertError(e).show();
-    });
-  }
-
-  var action = new PlugIn.Action(function(selection) {
-    const FerlatteLib = this.FerlatteLib;
-
+  var action = new PlugIn.Action(async function(selection) {
+    FerlatteLib = this.FerlatteLib;
     // To remove credentials, hold down control when selecting plug-in.
     if (app.controlKeyDown) {
-      let alert = new Alert("Confirmation Required",
-                            "Remove stored OpenAI API Key?");
-      alert.addOption("Remove");
-      alert.addOption("Cancel");
-      alert.show(buttonIndex => {
-        if (buttonIndex === 0) {
-          credentials.remove(serviceIdentifier);
-        }
-      });
+      FerlatteLib.requestOpenAIApiKeyRemoval(credentials);
     } else {
       let apiKey = FerlatteLib.openAIAPIKey(credentials);
       if (! apiKey) {
-        requestAPIKey();
+        FerlatteLib.requestOpenAIApiKey(credentials);
       } else {
         selection.tasks.forEach(task => {
           setEstimatedDuration(apiKey, task);
